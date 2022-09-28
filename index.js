6c9a4740497e8ec28ae7bd48e86b3d5b26dd38d1
hernpiblo <hpingleenet@gmail.com>
2022-09-28 13:22:58 +0800

    Add GitHub actions for EB CD

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const dataFilePath = "./data.json"
 
 // Base url
 app.get('/', (req, res) => {
-    res.send("Hello World from index.js with CICD")
+    res.send("Hello World from index.js with GA-EB CD")
 })
 
 // Get all user
